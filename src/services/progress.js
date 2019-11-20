1494a2cf1df067c33a32617b98370fb837588fa3
Nikolaj Tatti <nikolaj.tatti@helsinki.fi>
2019-11-20 05:26:41 +0200

    showing 2nd part points

--- src/services/progress.js
+++ src/services/progress.js
@@ -1,13 +1,17 @@
 import { fetchProgrammingProgress, getCachedUserDetails } from "./moocfi"
 import { fetchCrowdsorcererProgress } from "./crowdsorcerer"
 import { zip } from "../util/arrays"
+import { improveGroupName } from "../util/strings"
 import { fetchQuizProgress } from "./quiznator"
 import { fetchQuizzesProgress } from "./quizzes"
 
 export async function fetchProgress() {
   // await fetchQuizzesProgress()
-  const serviceIdentifiers = ["Quizzes"]
-  const progressesCollection = await Promise.all([fetchQuizzesProgress()])
+  const serviceIdentifiers = ["Programming exercises", "Quizzes"]
+  const progressesCollection = await Promise.all([
+    fetchProgrammingProgress(),
+    fetchQuizzesProgress(),
+  ])
   const userDetails = await getCachedUserDetails()
   const currentCourseVariant = userDetails?.extra_fields?.course_variant
   const progressByGroup = {}
@@ -16,14 +20,22 @@ export async function fetchProgress() {
     ([identifier, progresses]) => {
       console.log(JSON.stringify(progresses))
       progresses.forEach(progressEntry => {
-        if (!progressByGroup[progressEntry.group]) {
-          progressByGroup[progressEntry.group] = {}
+        const group = improveGroupName(
+          progressEntry.group.replace("Set", "Part"),
+        )
+        if (!progressByGroup[group]) {
+          progressByGroup[group] = {}
         }
-        progressByGroup[progressEntry.group][identifier] = progressEntry
+        progressByGroup[group][identifier] = progressEntry
       })
     },
   )
   const toBeDeleted = []
+  Object.entries(progressByGroup).forEach(([group, serviceEntries]) => {
+    if (!Object.keys(serviceEntries).find(o => o === "Programming exercises")) {
+      toBeDeleted.push(group)
+    }
+  })
 
   toBeDeleted.forEach(o => {
     delete progressByGroup[o]
